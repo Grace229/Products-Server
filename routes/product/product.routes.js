5679321eaf3c812c8324ed322589e8c7b8177e75
Grace229 <itiunggrace@gmail.com>
2021-11-23 17:39:19 +0100

    Get Products

--- routes/product/product.routes.js
+++ routes/product/product.routes.js
@@ -4,10 +4,12 @@ const upload = require('../../config/multerSetup');
 
 
 const createProduct = require('../../controllers/createProduct');
+const getAll = require('../../controllers/getProducts');
 
 
 router.route('/')
     .post(upload.single('postMedia'), createProduct)
+    .get(getAll);
 
 
 module.exports = router;
