b95cae6372a22e91f6f6824bf5df343b2d943b3a
Grace Itiung <59564004+Grace229@users.noreply.github.com>
2022-02-26 08:38:36 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const productRouter = require('./product/product.routes');
-
+router.get("/", (req, res) => {
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+ });
 const routers = (app) => {
     app.use('/api/v1/product', productRouter);
 }
